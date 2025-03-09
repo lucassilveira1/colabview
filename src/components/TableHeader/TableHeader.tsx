4a6d3cc6c25a0743a36505d5eae03207e3a2a304
Lucas Silveira <lucas.silveira0516@gmail.com>
2025-03-09 13:55:39 -0300

    feat: estrutura da tabela alterada e estilizando web

--- src/components/TableHeader/TableHeader.tsx
+++ src/components/TableHeader/TableHeader.tsx
@@ -1,11 +1,13 @@
 export const TableHeader = () => {
     return (
-        <div className="table-header">
-            <h2>FOTO</h2>
-            <h2>NOME</h2>
-            <h2>CARGO</h2>
-            <h2>DATA DE ADMISSÃO</h2>
-            <h2>TELEFONE</h2>
+        <div className="table-header-container">
+            <div className="table-header">
+                <h2>FOTO</h2>
+                <h2>NOME</h2>
+                <h2 className="desktop-only">CARGO</h2>
+                <h2 className="desktop-only">DATA DE ADMISSÃO</h2>
+                <h2 className="desktop-only">TELEFONE</h2>
+            </div>
         </div>
     );
 };
